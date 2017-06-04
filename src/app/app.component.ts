56e3a8f5c9a2d8e7e4082252e35e354315d7acee
NurlashKO <zh.nurlan96@gmail.com>
2017-06-05 04:58:02 +0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { JWord } from './models/JWord';
+import { WordsService } from './services/words.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  words: JWord[] = [];
+  data: JSON[];
+  currentWord: JWord;
+  displayWord: string;
+  pattern = /^[a-z]+$/;
+  showInfo: boolean = false;
+
+  @ViewChild("hiddenInput") userInput : any;
+
+  constructor(private wordsService: WordsService) {
+    wordsService.getWords().then(data => {
+      this.data = data
+      this.getWords();
+    });
+  }
+
+  private getWords() {
+    for (var i = 0; i < 10; i++) {
+      var pos = Math.floor(Math.random()*this.data.length)
+      var word = new JWord(this.data[pos]);
+      if (this.pattern.test(word.romaji)) {
+        this.words.push(word);
+      }
+    }
+    this.nextWord();
+  }
+
+  charAdded(value) {
+    value = value.toLowerCase();
+    if (value == this.currentWord.romaji) {
+      this.showInfo = true;
+    }
+    this.displayWord = this.currentWord.formatForDisplay(value);
+  } 
+
+  _keyPress(event: any) {
+    let inputChar = String.fromCharCode(event.charCode).toLowerCase();
+    
+    switch (inputChar.charCodeAt(0)) {
+      case 13: {
+        this.nextWord();
+        if (this.userInput.nativeElement.value != "") {
+          this.userInput.nativeElement.value = "";
+          this.userInput.nativeElement.update();
+        }
+        this.showInfo = false;
+        break;
+      }
+      case 32: {
+        this.showInfo = true;
+        break;
+      }
+    }
+
+    if (!this.pattern.test(inputChar)) {
+        // invalid character, prevent input
+        event.preventDefault();
+    }
+  }
+
+  nextWord() {
+    this.currentWord = this.words.pop();
+    this.displayWord = this.currentWord.hiragana;
+    if (this.words.length < 3)
+      this.getWords();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WordsService } from './services/words.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [WordsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
